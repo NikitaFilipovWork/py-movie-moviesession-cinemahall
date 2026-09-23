91b0d5309e9fa39ebadcaa0df8873e982aa4f78d
Nikita Filipov <nikitafilipov.work@gmail.com>
2026-09-23 22:30:48 +0300

    Added related name and updated logic in get_movies.

--- services/movie.py
+++ services/movie.py
@@ -4,22 +4,16 @@ from db.models import Movie
 
 
 def get_movies(
-        genres_ids: list[int] = None,
-        actors_ids: list[int] = None
+        genres_ids: list[int] | None = None,
+        actors_ids: list[int] | None = None
 ) -> QuerySet:
     queryset = Movie.objects.all()
 
-    if genres_ids and not actors_ids:
-        return queryset.filter(genres__id__in=genres_ids)
+    if genres_ids:
+        queryset = queryset.filter(genres__id__in=genres_ids)
 
-    if actors_ids and not genres_ids:
-        return queryset.filter(actors__id__in=actors_ids)
-
-    if genres_ids and actors_ids:
-        return queryset.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
+    if actors_ids:
+        queryset = queryset.filter(actors__id__in=actors_ids)
 
     return queryset
 
